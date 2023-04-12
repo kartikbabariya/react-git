dc6c925449d66189ad1a7c15fb900cdea926ba52
Kartik Babariya <74000666+kartikbabariya@users.noreply.github.com>
2023-04-12 14:08:26 +0100

    payment2 added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Login from './Components/Login/Login';
 import Login2 from './Components/Login/Login2';
 import Payment from './Components/Payment/Payment';
+import Payment2 from './Components/Payment/Payment2';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Login />
       <Login2 />
       <Payment />
+      <Payment2 />
     </div>
   );
 }
