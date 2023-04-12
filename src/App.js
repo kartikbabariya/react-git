959d78a8a1a3c96db67fbab97690e02f6a029e19
Kartik Babariya <74000666+kartikbabariya@users.noreply.github.com>
2023-04-12 14:03:43 +0100

    payment added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login/Login';
 import Login2 from './Components/Login/Login2';
+import Payment from './Components/Payment/Payment';
 
 function App() {
   return (
     <div className="App">
       <Login />
       <Login2 />
+      <Payment />
     </div>
   );
 }
