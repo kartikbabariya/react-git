89d976e73c6370694e64b8b71e414059a4b6b143
Kartik Babariya <74000666+kartikbabariya@users.noreply.github.com>
2023-04-12 14:06:58 +0100

    login3 added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login/Login';
 import Login2 from './Components/Login/Login2';
+import Login3 from './Components/Login/Login3';
 
 function App() {
   return (
     <div className="App">
       <Login />
       <Login2 />
+      <Login3 />
     </div>
   );
 }
---
+++ src/Components/Login/Login3.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login3 = () => {
+  return (
+    <div>Login3</div>
+  )
+}
+
+export default Login3
